207fd5351690810e1729bd5dcc20772042317260
humphrey-code <humphrex@gmail.com>
2019-08-25 14:10:39 +0300

    apply Event Listeners to bring a toggle effect,

---
+++ js/scripts.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+  $("#design_img").click(function(){
+    $("#design_img").toggle();
+    $("#design_par").toggle();
+  });
+});
+
+$(document).ready(function(){
+  $("#design_par").click(function(){
+    $("#design_img").toggle();
+    $("#design_par").toggle();
+  });
+});
+
+$(document).ready(function(){
+  $("#dev_img").click(function(){
+    $("#dev_img").toggle();
+    $("#dev_par").toggle();
+  });
+});
+
+$(document).ready(function(){
+  $("#dev_par").click(function(){
+    $("#dev_img").toggle();
+    $("#dev_par").toggle();
+  });
+});
+
+$(document).ready(function(){
+  $("#prod_img").click(function(){
+    $("#prod_img").toggle();
+    $("#prod_par").toggle();
+  });
+});
+
+$(document).ready(function(){
+  $("#prod_par").click(function(){
+    $("#prod_img").toggle();
+    $("#prod_par").toggle();
+  });
+});
